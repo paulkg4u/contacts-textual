7352a5889f690f2e841ca951138effc5c5383dba
Paul Kottalil George <paul@peers-solutions.com>
2024-11-03 22:21:50 +0100

    add ui for actions

--- contacts/tui.py
+++ contacts/tui.py
@@ -1,7 +1,7 @@
-from textual.app import App
+from textual.app import App, on
 from textual.widgets import (
     Footer, Header, Button, Label,
-    DataTable, Static
+    DataTable, Static, Input
 )
 from textual.containers import Grid, Horizontal, Vertical
 from textual.screen import Screen
@@ -22,6 +22,52 @@ class ContactsApp(App):
         super().__init__()
         self.db = db
 
+    @on(Button.Pressed, "#add")
+    def action_add(self):
+        def check_contact(contact_data):
+            if contact_data:
+                self.db.add_contact(contact_data)
+                id, *contact = self.db.get_last_contact()
+                self.query_one(DataTable).add_row(
+                    *contact, key=id
+                )
+
+        self.push_screen(InputDialog(), check_contact)
+
+    @on(Button.Pressed, "#delete")
+    def action_delete(self):
+        contacts_list = self.query_one(DataTable)
+        row_key, _ = contacts_list.coordinate_to_cell_key(
+            contacts_list.cursor_coordinate
+        )
+
+        def check_answer(accepted):
+            if accepted and row_key:
+                self.db.delete_contact(contact_id=row_key.value)
+                contacts_list.remove_row(row_key)
+
+        name = contacts_list.get_row(row_key)[0]
+
+        self.push_screen(
+            QuestionDialog(
+                f"Are you sure you want to delete {name}?",
+            ),
+            check_answer
+        )
+
+    @on(Button.Pressed, "#clear")
+    def action_clear(self):
+        def check_answer(accepted):
+            if accepted:
+                self.db.clear_contacts()
+                self.query_one(DataTable).clear()
+
+        self.push_screen(
+            QuestionDialog(
+                "Are you sure you want to clear all contacts?",
+            ),
+            check_answer
+        )
 
     def compose(self):
         yield Header()
@@ -93,3 +139,44 @@ class QuestionDialog(Screen):
             self.dismiss(True)
         elif event.button.id == "no":
             self.dismiss(False)
+
+
+class InputDialog(Screen):
+
+    def compose(self):
+        yield Grid(
+            Label("Add a new contact", id="title"),
+            Label("Name", classes="label"),
+            Input(
+                placeholder="Contact Name",
+                classes="input",
+                id="name"
+            ),
+            Label("Email", classes="label"),
+            Input(
+                placeholder="Contact Email",
+                classes="input",
+                id="email"
+            ),
+            Label("Phone", classes="label"),
+            Input(
+                placeholder="Contact Phone",
+                classes="input",
+                id="phone"
+            ),
+            Static(),
+
+            Button("Save", variant="success", id="save"),
+            Button("Cancel", variant="warning", id="cancel"),
+            id="input-dialog"
+        )
+
+    def on_button_pressed(self, event):
+        if event.button.id == "save":
+            self.dismiss((
+                self.query_one("#name", Input).value,
+                self.query_one("#email", Input).value,
+                self.query_one("#phone", Input).value
+            ))
+        elif event.button.id == "cancel":
+            self.dismiss()
